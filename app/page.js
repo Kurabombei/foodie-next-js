46afaf8aaab0c1149aecef693afba917de51e3f0
Ivan Volos (new) <ivan.volos1@gmail.com>
2025-01-16 00:05:26 +0200

    Add about page. Add Header file.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+import Header from "@mentee-dash/app/components/Header";
+
 export default function Home() {
+    console.log("Hello!!!")
   return (
       <main>
-        <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-        <h1>Welcome to this NextJS Course!</h1>
+        <Header />
         <p>🔥 Let&apos;s get started! 🔥</p>
+          <p><Link href="/about">About page</Link></p>
       </main>
   );
 }
