a86cdef9c417536a77d8a55ea078c74695e4b15e
Ivan Volos (new) <ivan.volos1@gmail.com>
2025-01-16 15:22:51 +0200

    Add needed assets and images for project. Update project structure.

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import Link from "next/link";
-import Header from "@foodie-dash/app/components/Header";
-
 export default function Home() {
-    console.log("Hello!!!")
   return (
-      <main>
-        <Header />
-        <p>🔥 Let&apos;s get started! 🔥</p>
-          <p><Link href="/about">About page</Link></p>
-          <p><Link href="/blog">Blog page</Link></p>
-      </main>
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+    </main>
   );
 }
