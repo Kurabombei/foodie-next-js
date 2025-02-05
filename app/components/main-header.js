685223815fa69d1a79d3d65f6d7aab5c307846a3
Ivan Volos (new) <ivan.volos1@gmail.com>
2025-02-05 12:40:21 +0200

    Optimize logo image with Next.js Image.

--- app/components/main-header.js
+++ app/components/main-header.js
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 import logoImg from "@foodie-dash/assets/logo.png"
 import classes from './main-header.module.css'
+import Image from "next/image";
 
 export default function MainHeader() {
 	return <header className={classes.header}>
 		<Link className={classes.logo} href='/'>
-			<img src={logoImg.src} alt="A plate with food on it."/>
+			<Image
+				src={logoImg}
+				priority
+				alt="A plate with food on it."
+			/>
 			NextLevel Food
 		</Link>
 		<nav className={classes.nav}>
